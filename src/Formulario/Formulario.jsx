09b28ca6fb13a7bebbde41a309e1a52d1fe42247
sebastiangib <sebastianberrio0627@gmail.com>
2023-11-16 22:15:44 -0500

    form

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -2,21 +2,53 @@ export function Formulario() {
   return (
     <>
       <form>
-        <div className="row">
+        <div className="row letra">
           <div className="col-12 col-md-6">
             <div class="mb-3">
               <label class="form-label">
-                Usuario
+                Nombre <i class="fa-solid fa-user"></i>
               </label>
-              <input type="password" className="form-control"/>
+              <input type="text" className="form-control"/>
             </div>
           </div>
           <div className="col-12 col-md-6">
           <div class="mb-3">
               <label class="form-label">
-                Telefono
+                Apellido <i class="fa-solid fa-user"></i>
+              </label>
+              <input type="text" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+          <div class="mb-3">
+              <label class="form-label">
+                Telefono <i class="fa-solid fa-phone"></i>
               </label>
               <input type="number" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+          <div class="mb-3">
+              <label class="form-label">
+                Numero de Personas <i class="fa-solid fa-users"></i>
+              </label>
+              <input type="number" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+          <div class="mb-3">
+              <label class="form-label">
+                Fecha Inicial <i class="fa-solid fa-calendar-days"></i>
+              </label>
+              <input type="date" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+          <div class="mb-3">
+              <label class="form-label">
+                Fecha Final <i class="fa-solid fa-calendar-days"></i>
+              </label>
+              <input type="date" className="form-control"/>
             </div>
           </div>
         </div>
